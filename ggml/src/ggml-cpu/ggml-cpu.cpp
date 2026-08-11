7c6aff84c473b50817ba4f258918a81eb6d58789
Loveneet Nigam <lnigam@nvidia.com>
2026-08-11 13:28:46 +0530

    Adding changes for supporting RS-rollback in CPU. Also added test-backend-ops for cpu and cuda

--- ggml/src/ggml-cpu/ggml-cpu.cpp
+++ ggml/src/ggml-cpu/ggml-cpu.cpp
@@ -472,7 +472,7 @@ static bool ggml_backend_cpu_device_supports_op(ggml_backend_dev_t dev, const st
         case GGML_OP_CONV_2D:
             return ggml_is_contiguous(op->src[0]);
         case GGML_OP_SSM_SCAN:
-            return ggml_get_op_params_i32(op, 0) == 1;
+            return ggml_get_op_params_i32(op, 0) == 1 || op->src[3]->ne[0] == 1;
         default:
             return true;
     }
